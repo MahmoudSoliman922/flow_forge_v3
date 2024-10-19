0ea914be7831864af73b5277a6a87b819e5c9154
soliman <mahmoudsoliman922@gmail.com>
2024-10-19 05:42:56 +0000

    fix vite for port forwarding

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Listen on all network interfaces
+    host: true,
+    // Specify the port Vite should use, if necessary
+    port: 5173, // Optional: Use the port your app should run on
+  },
 });
